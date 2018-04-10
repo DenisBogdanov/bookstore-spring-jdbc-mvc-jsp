1212d626235dfc8c3b3e5e121f22cec82b9fba9a
Denis Bogdanov <bogdanov.math@gmail.com>
2018-04-10 22:39:13 +0300

    Add configuration and welcome page.

---
+++ src/main/java/ru/bogdanium/bookstore/controller/HomeController.java
@@ -0,0 +1,18 @@
+package ru.bogdanium.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "BookStore!");
+        model.addAttribute("motto", "Welcome people!");
+
+        return "welcome";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
+</head>
+<body>
+
+<section>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>${greeting}</h1>
+            <p>${motto}</p>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
