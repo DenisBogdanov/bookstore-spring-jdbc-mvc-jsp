1eaa34b04340f570a33ed395e7a874416297b57c
Denis Bogdanov <bogdanov.math@gmail.com>
2018-04-12 13:21:38 +0300

    Add static resources (images).

--- src/main/java/ru/bogdanium/bookstore/controller/HomeController.java
+++ src/main/java/ru/bogdanium/bookstore/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String welcome(Model model) {
-        model.addAttribute("greeting", "BookStore!");
+        model.addAttribute("greeting", "BookStore");
         model.addAttribute("motto", "Welcome people!");
 
         return "welcome";
